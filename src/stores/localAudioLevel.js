6d98a9f4829ffa39580d30b40aefffe40f8173eb
Duane Johnson <duane.johnson@gmail.com>
2021-03-12 16:50:20 -0700

    Formatting

--- src/stores/localAudioLevel.js
+++ src/stores/localAudioLevel.js
@@ -5,24 +5,28 @@ import hark from "hark";
 let harkInstance;
 let audioVolume = 0;
 
-export const localAudioLevel = derived([localStream], ([$stream], set) => {
-  if ($stream && $stream.getAudioTracks().length) {
-    harkInstance = hark($stream, { play: false });
+export const localAudioLevel = derived(
+  [localStream],
+  ([$stream], set) => {
+    if ($stream && $stream.getAudioTracks().length) {
+      harkInstance = hark($stream, { play: false });
 
-    harkInstance.on("volume_change", (dBs, threshold) => {
-      let volume = Math.pow(10, dBs / 120);
-      if (volume >= 1 || isNaN(volume)) volume = 0;
-      if (volume !== audioVolume) {
-        set(volume);
-        audioVolume = volume;
-      }
-    });
-  }
-
-  return () => {
-    if (harkInstance) {
-      harkInstance.stop();
-      harkInstance = null;
+      harkInstance.on("volume_change", (dBs, threshold) => {
+        let volume = Math.pow(10, dBs / 120);
+        if (volume >= 1 || isNaN(volume)) volume = 0;
+        if (volume !== audioVolume) {
+          set(volume);
+          audioVolume = volume;
+        }
+      });
     }
-  };
-}, 0);
+
+    return () => {
+      if (harkInstance) {
+        harkInstance.stop();
+        harkInstance = null;
+      }
+    };
+  },
+  0
+);
