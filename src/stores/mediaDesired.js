f3647dbc6f78bd5a3e82d893fe76ceb1714e4035
Duane Johnson <duane.johnson@gmail.com>
2021-12-28 14:37:12 -0700

    Revert "Add local storage for mediaDesired state"
    This reverts commit 2dda85392be734c48870a5ecad0711110a863031.

--- src/stores/mediaDesired.js
+++ src/stores/mediaDesired.js
@@ -1,9 +1,6 @@
-import { writable } from "svelte-local-storage-store";
+import { writable } from "svelte/store";
 
 /**
  * Represents the current state of the user's desire to have audio or video
  */
-export const mediaDesired = writable("video-mirror.mediaDesired", {
-  audio: true,
-  video: true,
-});
+export const mediaDesired = writable({ audio: true, video: true });
