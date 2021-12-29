01ef451e133a1ecb98ff07ab0dafe89ca1360197
Duane Johnson <duane.johnson@gmail.com>
2021-12-28 19:10:38 -0700

    Convert to Typescript project

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,51 @@
+import image from "@rollup/plugin-image";
+import nodePolyfills from "rollup-plugin-node-polyfills";
+import commonjs from "@rollup/plugin-commonjs";
 import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
-import image from '@rollup/plugin-image'
-import nodePolyfills from "rollup-plugin-node-polyfills";
-import commonjs from '@rollup/plugin-commonjs';
-import { terser } from "rollup-plugin-terser";
+import sveltePreprocess from "svelte-preprocess";
+import typescript from "@rollup/plugin-typescript";
+import execute from "rollup-plugin-execute";
 import pkg from "./package.json";
 
 const name = pkg.name
   .replace(/^\w/, (m) => m.toUpperCase())
   .replace(/-\w/g, (m) => m[1].toUpperCase());
 
-export default {
-  input: "src/index.js",
-  output: [
-    { file: pkg.module, format: "es" },
-    { file: pkg.main, format: "umd", name },
-  ],
-  plugins: [svelte(), nodePolyfills(), commonjs(), resolve(), image(), terser()],
-};
+console.log("pkg.name", name);
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: pkg.module,
+        format: "es",
+        sourcemap: true,
+      },
+      {
+        file: pkg.main,
+        format: "umd",
+        name,
+        sourcemap: true,
+        plugins: [
+          // we only want to run this once, so we'll just make it part of this output's plugins
+          execute([
+            "tsc --outDir ./dist --declaration",
+            "node scripts/preprocess.js",
+          ]),
+        ],
+      },
+    ],
+    plugins: [
+      svelte({
+        preprocess: sveltePreprocess(),
+      }),
+      nodePolyfills(),
+      commonjs(),
+      resolve(),
+      image(),
+      typescript(),
+    ],
+  },
+];
